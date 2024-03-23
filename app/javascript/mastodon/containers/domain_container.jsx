3b343cc617d0929cb1a11d6333a654a1bdda1c36
mogaminsk <mgmnjp@icloud.com>
2024-03-23 13:19:02 +0900

    remove unused onBlockDomain

--- app/javascript/mastodon/containers/domain_container.jsx
+++ app/javascript/mastodon/containers/domain_container.jsx
@@ -1,14 +1,9 @@
-import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
+import { injectIntl } from 'react-intl';
 
 import { connect } from 'react-redux';
 
-import { blockDomain, unblockDomain } from '../actions/domain_blocks';
-import { openModal } from '../actions/modal';
+import { unblockDomain } from '../actions/domain_blocks';
 import { Domain } from '../components/domain';
-
-const messages = defineMessages({
-  blockDomainConfirm: { id: 'confirmations.domain_block.confirm', defaultMessage: 'Block entire domain' },
-});
 
 const makeMapStateToProps = () => {
   const mapStateToProps = () => ({});
@@ -16,18 +11,7 @@ const makeMapStateToProps = () => {
   return mapStateToProps;
 };
 
-const mapDispatchToProps = (dispatch, { intl }) => ({
-  onBlockDomain (domain) {
-    dispatch(openModal({
-      modalType: 'CONFIRM',
-      modalProps: {
-        message: <FormattedMessage id='confirmations.domain_block.message' defaultMessage='Are you really, really sure you want to block the entire {domain}? In most cases a few targeted blocks or mutes are sufficient and preferable. You will not see content from that domain in any public timelines or your notifications. Your followers from that domain will be removed.' values={{ domain: <strong>{domain}</strong> }} />,
-        confirm: intl.formatMessage(messages.blockDomainConfirm),
-        onConfirm: () => dispatch(blockDomain(domain)),
-      },
-    }));
-  },
-
+const mapDispatchToProps = dispatch => ({
   onUnblockDomain (domain) {
     dispatch(unblockDomain(domain));
   },
